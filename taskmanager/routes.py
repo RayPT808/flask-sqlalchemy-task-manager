fe1e70e6028136160ddfb6995c7b828446b3815b
RayPT808 <qiauxirt@students.codeinstitute.net>
2024-09-11 14:16:41 +0000

    Fixed syntax errors.

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect, url_for, 
+from flask import render_template, request, redirect, url_for
 from  taskmanager import app, db
 from taskmanager.models import Category, Task
 
@@ -15,7 +15,7 @@ def categories():
 
 @app.route("/add_category", methods=["GET", "POST"])
 def add_category():
-    if request.method == "POST"
+    if request.method == "POST":
         category = Category(category_name=request.form.get("category_name"))
         db.session.add(category)
         db.session.commit()
