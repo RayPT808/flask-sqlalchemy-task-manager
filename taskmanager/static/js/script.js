ba6dd4b6430a7e4a73820812153598cbd6af55d3
RayPT808 <qiauxirt@students.codeinstitute.net>
2024-09-12 12:28:11 +0000

    Added task selector function.

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(elems, options);
+
+
+    // datepicker initalization
+    var datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+      i18n: {done: "Select" }
+
+    });
+    // select initalization
+    var selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
   });
